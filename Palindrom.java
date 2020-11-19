03f830f9c79b1404a7aeb070efb9026da8509854
Simona Anchova <anchovasimona@yahoo.com>
2020-11-19 10:37:35 +0000

    Palindrom.java

--- Palindrom.java
+++ Palindrom.java
@@ -1,21 +1,22 @@
 package algoritmi20;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class Palin {
-    public static int isItPalindrome(DLL<Integer> list){
+    public static int isItPalindrom (DLL<Integer> list) {
         DLLNode<Integer> first = list.getFirst();
         DLLNode<Integer> last = list.getLast();
-        for (int i = 0; i < list.length() / 2; i++){
-            if (first.element != last.element){
-                return -1;
+
+        for (int i = 0; i < list.length(); i++) {
+            if (first.element == last.element) {
+                return 1;
             }
             first = first.succ;
             last = last.pred;
         }
-        return 1;
+        return -1;
     }
 
     public static void main (String [] args) throws IOException {
@@ -25,11 +26,11 @@ public class Palin {
         String vtor = stdin.readLine();
         String [] niza = vtor.split(" ");
         DLL<Integer> list = new DLL<Integer>();
-        for (int i = 0; i < niza.length; i++){
+        for (int i = 0; i < niza.length; i++) {
             Integer broj = Integer.parseInt(niza[i]);
             list.insertLast(broj);
         }
-        System.out.println(isItPalindrome(list));
+        System.out.println(isItPalindrom(list));
     }
 }
 
