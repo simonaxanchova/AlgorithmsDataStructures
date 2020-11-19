7d151bf4d2a932fdd34859d06106077b4cc80116
Simona Anchova <anchovasimona@yahoo.com>
2020-11-19 12:33:36 +0000

    SLLKompanija.java

--- SLLKompanija.java
+++ SLLKompanija.java
@@ -8,24 +8,24 @@ class Vraboten {
     private int ID;
     private int plata;
 
-    public Vraboten (int ID, int plata){
+    public Vraboten(int ID, int plata) {
         this.ID = ID;
         this.plata = plata;
     }
 
-    public int getID(){
+    public int getID() {
         return ID;
     }
 
-    public void setID(int iD){
+    public void setID(int ID) {
         this.ID = ID;
     }
 
-    public int getPlata(){
+    public int getPlata() {
         return plata;
     }
 
-    public void setPlata(int plata){
+    public void setPlata(int plata) {
         this.plata = plata;
     }
 }
@@ -35,43 +35,43 @@ public class SLLKompanija {
         SLL<Vraboten> list = new SLL<Vraboten>();
         BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(stdin.readLine());
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             list.insertLast(new Vraboten(Integer.parseInt(stdin.readLine()), Integer.parseInt(stdin.readLine())));
         }
 
         Integer sallary = Integer.parseInt(stdin.readLine());
-        SLLNode<Vraboten> tmp = list.getFirst();
-        while (tmp != null){
-            if (tmp.element.getPlata() < sallary){
-                SLLNode<Vraboten> tmp2 = tmp;
-                list.delete(tmp2);
+        SLLNode<Vraboten> node = list.getFirst();
+        while (node != null) {
+            if (node.element.getPlata() < sallary) {
+                SLLNode<Vraboten> tmp = node;
+                list.delete(tmp);
             }
-            tmp = tmp.succ;
+            node = node.succ;
         }
 
-        int i = 0;
-        for (int j = 0; j < list.length() - 1; j++){
+        for (int i = 0; i < list.length(); i++) {
             SLLNode<Vraboten> currentNode = list.getFirst();
-            for (i = 0; i < list.length() - 1 - j; i++){
-                if (currentNode.element.getID() < currentNode.succ.element.getID()){
-                    list.insertAfter(currentNode.element, currentNode.succ);
-                    SLLNode<Vraboten> tmp2 = currentNode;
-                    currentNode = currentNode.succ;
-                    list.delete(tmp2);
+            Vraboten auxNode;
+            for (int j = 0; j < list.length() - 1; j++) {
+                if (currentNode.element.getID() < currentNode.succ.element.getID()) {
+                    auxNode = currentNode.element;
+                    currentNode.element = currentNode.succ.element;
+                    currentNode.succ.element = auxNode;
                 }
                 currentNode = currentNode.succ;
             }
         }
 
-        tmp = list.getFirst();
-        if (tmp == null){
+        node = list.getFirst();
+        if (node == null) {
             System.out.println("nema");
         }
 
-        while (tmp != null){
-            System.out.println(tmp.element.getID() + " " + tmp.element.getPlata());
-            tmp = tmp.succ;
+        while (node != null) {
+            System.out.println(node.element.getID() + " " + node.element.getPlata());
+            node = node.succ;
         }
 
     }
 }
+
